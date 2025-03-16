f1d8459343090c7a3b2f3a861d570dfeb60196b8
danilo631 <defreitasfernandesdanilo81@gmail.com>
2025-03-16 14:31:55 -0300

    Update login.js

--- src/components/login.js
+++ src/components/login.js
@@ -1,1 +1,15 @@
+import { auth } from "../firebase";
+
+const login = () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  auth.signInWithEmailAndPassword(email, password)
+    .then(() => {
+      window.location.href = "lobby.html"; // Redireciona para o lobby
+    })
+    .catch((error) => alert(error.message));
+};
+
+document.querySelector("#loginBtn").addEventListener("click", login);
 
